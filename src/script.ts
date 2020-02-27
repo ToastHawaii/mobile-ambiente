92d52581ce6f7582b0e08d743ccf1d6a12d0a8d7
ToastHawaii <mar.pel@protonmail.com>
2020-02-27 19:48:13 +0100

    test

--- src/script.ts
+++ src/script.ts
@@ -3,3 +3,14 @@ import { init, categories } from "./Boombox";
 $(document).ready(() => {
   init(".main", categories);
 });
+
+const jsmediatags = require("jsmediatags");
+
+jsmediatags.read("https://media.zottelig.ch/ambiente/audio/car/car-horn.mp3", {
+  onSuccess: function(tag: any) {
+    console.log(tag);
+  },
+  onError: function(error: any) {
+    console.log(":(", error.type, error.info);
+  }
+});
