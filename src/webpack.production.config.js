10746c6b578521c03177b4220696496d3861404f
ToastHawaii <mar.pel@protonmail.com>
2020-08-31 10:12:39 +0200

    remove workbox and offline support

--- src/webpack.production.config.js
+++ src/webpack.production.config.js
@@ -21,41 +21,7 @@ module.exports = {
       template: "src/inspiration.html",
       filename: "inspiration.html"
     }),
-    new WorkboxPlugin.GenerateSW({
-      // these options encourage the ServiceWorkers to get in there fast
-      // and not allow any straggling "old" SWs to hang around
-      clientsClaim: true,
-      skipWaiting: true,
-      additionalManifestEntries: [
-        "/docs/index.html",
-        "/semantic/dist/semantic.min.css",
-        "/semantic/dist/semantic.min.js",
-        "https://cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.min.js",
-        "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css"
-      ],
-      exclude: [/\.(?:yml|md)$/, /_layouts\/.*/, /\.(?:png|jpg|jpeg|svg)$/],
 
-      // Define runtime caching rules.
-      runtimeCaching: [
-        {
-          // Match any request that ends with .png, .jpg, .jpeg or .svg.
-          urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
-
-          // Apply a cache-first strategy.
-          handler: "CacheFirst",
-
-          options: {
-            // Use a custom cache name.
-            cacheName: "images",
-
-            // Only cache 10 images.
-            expiration: {
-              maxEntries: 10
-            }
-          }
-        }
-      ]
-    }),
     new webpack.NamedModulesPlugin(),
     new webpack.DefinePlugin({
       "process.env.NODE_ENV": JSON.stringify("production")
